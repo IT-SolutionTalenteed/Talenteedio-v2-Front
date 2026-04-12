b1db4fdf3ba175c10ce9892f3f595ba88265fd20
Happy-Franck <solofonirina35@gmail.com>
2026-04-12 15:15:14 +0300

    feat(evenement-service): add parseCv and matchingOffresGlobal endpoints

--- src/services/talent/evenementService.js
+++ src/services/talent/evenementService.js
@@ -3,6 +3,10 @@ import api from '../api.js'
 export const evenementService = {
   getAll: () => api.get('/talent/evenements'),
 
+  parseCv: (formData) => api.post('/talent/cv/parse', formData, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+  }),
+
   matching: (evenementId, formData) => api.post(`/talent/evenements/${evenementId}/matching`, formData, {
     headers: { 'Content-Type': 'multipart/form-data' },
   }),
