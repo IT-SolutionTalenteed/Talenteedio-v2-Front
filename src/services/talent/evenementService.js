4bc4931ddcf0a560a5c8e9749b6d963d9d7eebf5
Happy-Franck <solofonirina35@gmail.com>
2026-04-12 14:48:42 +0300

    feat(matching): ajouter service matchingOffresGlobal

--- src/services/talent/evenementService.js
+++ src/services/talent/evenementService.js
@@ -2,8 +2,13 @@ import api from '../api.js'
 
 export const evenementService = {
   getAll: () => api.get('/talent/evenements'),
+
   matching: (evenementId, formData) => api.post(`/talent/evenements/${evenementId}/matching`, formData, {
-    headers: { 'Content-Type': 'multipart/form-data' }
+    headers: { 'Content-Type': 'multipart/form-data' },
+  }),
+
+  matchingOffresGlobal: (formData) => api.post('/talent/matching-offres', formData, {
+    headers: { 'Content-Type': 'multipart/form-data' },
   }),
 }
 
