dd961588e8ab60f4c11a7540b05ff21e7dad332f
joann002 <joannrazafinimanana159@gmail.com>
2026-05-20 19:28:03 +0300

    chore(vite.config): remove unused vite-plugin-imagemin dependency
    - Eliminated the vite-plugin-imagemin from the Vite configuration to streamline project dependencies and reduce bloat.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import viteImagemin from 'vite-plugin-imagemin'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,33 +10,6 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    viteImagemin({
-      // Compression pour PNG
-      optipng: {
-        optimizationLevel: 7
-      },
-      // Compression pour JPEG
-      mozjpeg: {
-        quality: 80
-      },
-      // Compression pour SVG
-      svgo: {
-        plugins: [
-          {
-            name: 'removeViewBox',
-            active: false
-          },
-          {
-            name: 'removeEmptyAttrs',
-            active: true
-          }
-        ]
-      },
-      // Génération de WebP (format moderne, plus léger)
-      webp: {
-        quality: 80
-      }
-    })
   ],
   resolve: {
     alias: {
