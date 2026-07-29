4fb537f9421fc32b712aa385429bbc923cc77ab0
Happy-Franck <solofonirina35@gmail.com>
2026-07-29 12:47:23 +0300

    fix(home): point observatory CTA to passport questionnaire URL

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -26,9 +26,9 @@
             <a href="https://www.eventbrite.fr/e/billets-africa-talent-summit-luxembourg-1993826693312?aff=ebdsoporgprofile" target="_blank" rel="noopener" class="btn-primary">
               {{ t('home.hero.subscribe') }}
             </a>
-            <router-link to="/africa-talent-observatory" class="btn-secondary">
+            <a href="https://africatalentsummit.com/passport-questionnaire/" target="_blank" rel="noopener" class="btn-secondary">
               {{ t('home.hero.joinObservatory') }}
-            </router-link>
+            </a>
           </div>
 
           <!-- Countdown -->
